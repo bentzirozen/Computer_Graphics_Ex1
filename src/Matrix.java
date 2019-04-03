fc3a4647f200df0098222e4f33d8686dda080877
Sapir <sapirzilbershtein@gmail.com>
2019-04-03 11:23:49 +0300

    sapir

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,21 +1,20 @@
-public class Matrix{
+public class Matrix {
 
 //State Variables: Private state varibles were created so that they could not be accidently accessed. Each iteration of the Matrix Class with have it's own m,numberOfRows and numberOfColumns.
 
     private double[][] m;
-    private int numberOfRows; private int numberOfColumns;
+    private int numberOfRows;
+    private int numberOfColumns;
 
 //Constructors: Two constructors were created. The first, takes two ints as parameters for the shape of the matrix and initializes the elements to 0. The second has no parameters, but instead asks the user to supply the shape and element values via scanner.
 
-    public Matrix(int rows, int columns)
-    {
+    public Matrix(int rows, int columns) {
         m = new double[rows][columns];
 
-        for(int i = 0; i < rows; i++)
-        {
-            for(int j = 0; j < columns; j++)
-            {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
                 m[i][j] = 0;
             }
         }
     }
+}
