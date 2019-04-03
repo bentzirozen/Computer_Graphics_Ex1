0582c7b01b057bac3036c537226d768ba6411c77
Sapir <sapirzilbershtein@gmail.com>
2019-04-03 15:54:13 +0300

    matrix class

--- src/Matrix.java
+++ src/Matrix.java
@@ -8,7 +8,9 @@ public class Matrix {
 
 //Constructors: Two constructors were created. The first, takes two ints as parameters for the shape of the matrix and initializes the elements to 0. The second has no parameters, but instead asks the user to supply the shape and element values via scanner.
 
-    public Matrix(int rows, int columns) {
+    public Matrix(int rows , int columns) {
+        rows = 4;
+        columns = 4;
         m = new double[rows][columns];
 
         for (int i = 0; i < rows; i++) {
@@ -16,5 +18,32 @@ public class Matrix {
                 m[i][j] = 0;
             }
         }
+    }
+    private int getRows(){return 4;}
+    private int getColumns(){return 4;}
+    private double getElement(int rows, int columns){return m[rows][columns];}
+    private void assignElement(double value, int i, int j){m[i][j] = value;}
+
+    public static Matrix multiply(Matrix x, Matrix y)
+    {
+        //z is the new matrix created.
+        Matrix z = new Matrix(x.getRows(), y.getColumns());
+        double value;
+        for(int i = 0; i < x.getRows(); i++)
+        {
+            for(int j = 0; j < y.getColumns(); j++)
+            {
+                double sum = 0;
+                for(int k = 0; k < x.getRows(); k++)
+                {
+                    sum += x.getElement(i,k) * y.getElement(k,j);
+                }
+                value = sum;
+                //put the value in the correct cell.
+                z.assignElement(value,i,j);
+            }
+        }
+        //return the new matrix.
+        return z;
     }
 }
