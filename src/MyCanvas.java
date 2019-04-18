58a2b444b750385b0122c0214c5e6a97c7b52f0f
Benzion Rozen <benzion.rozen@live.biu.ac.il>
2019-04-18 17:57:13 +0300

    filled some methods on MyCanvas

--- src/MyCanvas.java
+++ src/MyCanvas.java
@@ -1,3 +1,5 @@
+import com.sun.xml.internal.bind.v2.TODO;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -14,7 +16,14 @@ public class MyCanvas extends Canvas implements KeyListener, MouseListener, Mous
 
     private Scene scene;
     private View view;
+    private Tranforamtions tranforamtions;
+    private int screenWidth,screenHeight;
+    private Point3D center;
+    private Matrix VM1, P, CT, TT, T1, T2, AT, VM2;
     private boolean clip=false;
+    private double px,py; //x and y after mouse press
+    private double dx,dy; //x and y after mouse drag
+    private char currentOper;
 
     Point pStart, pEnd;
     boolean bFlag = false;
@@ -28,11 +37,13 @@ public class MyCanvas extends Canvas implements KeyListener, MouseListener, Mous
             this.view.readViw(new File("ex1.viw"));
             this.scene = new Scene();
             this.scene.readScn(new File("ex1.scn"));
-
+            this.tranforamtions = new Tranforamtions();
             setSize(view.getScreenWidth()+ MARGIN, view.getScreenHeight() + MARGIN);
             addMouseListener(this);
             addMouseMotionListener(this);
-
+            addKeyListener(this);
+            this.center = new Point3D((screenWidth/2)+20,(screenHeight/2)+20,0);
+            
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
@@ -42,15 +53,41 @@ public class MyCanvas extends Canvas implements KeyListener, MouseListener, Mous
         this.scene.from3Dto2D();
         List<Line> edgeList = this.scene.getEdgeList();
         g.setColor(Color.blue);
-
-        // none sense values
-
-        for (Line e : edgeList) {
-            g.drawLine(50 * (int) e.getStart().getX(), 70 * (int) e.getStart().getY(),
-                    100 * (int) e.getEnd().getX(), 60 * (int) e.getEnd().getY());
-        }
     }
 
+    /**
+     * returns on which square you pressed , rotate , translate etc.
+     * @param x x pos of mouse
+     * @param y y pos of mouse
+     * @return
+     */
+    private char wherePressed(double x,double y){
+        //not on borders
+        if (x < 20 || y < 20 || x > screenWidth + 20 || y > screenHeight + 20) {
+            return '0';
+        } else if(x >= screenWidth / 3 +20 && x <= 2 * screenWidth / 3 + 20 && y >= screenHeight / 3 +20 && y < 2 * screenHeight / 3 + 20) {
+            return 't';
+        } else if((x < screenWidth / 3 +20 || x > 2 * screenWidth / 3 + 20) && (y < screenHeight / 3 + 20 || y> 2 * screenHeight / 3 +20)) {
+            return 'r';
+        } else{
+            return 's';
+        }
+    }
+    private void oper(){
+        switch(this.currentOper){
+            case 't':
+                CT = tranforamtions.Translation(dx - px, dy - py,0);
+                break;
+            case 'R':
+                //TODO rotation
+                break;
+            case 'S':
+                //TODO scale
+                break;
+            default:
+                break;
+        }
+    }
 
         @Override
     public void keyTyped(KeyEvent e) {
@@ -99,7 +136,11 @@ public class MyCanvas extends Canvas implements KeyListener, MouseListener, Mous
 
     @Override
     public void mouseDragged(MouseEvent e) {
-
+        System.out.println("Mouse Dragged");
+        dx = e.getX();
+        dy = e.getY();
+        oper();
+        this.repaint();
     }
 
     @Override
@@ -109,7 +150,10 @@ public class MyCanvas extends Canvas implements KeyListener, MouseListener, Mous
 
     @Override
     public void mousePressed(MouseEvent e) {
-
+        System.out.println("Mouse Pressed");
+        px = e.getX();
+        py = e.getY();
+        currentOper = wherePressed(px, py);
     }
 
     @Override
